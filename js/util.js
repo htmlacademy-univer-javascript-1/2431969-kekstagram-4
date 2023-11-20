742d4f7957e788fe0a9b61cf9178a37d2aaa1c9f
helensusanina <alyona.susanina@mail.ru>
2023-11-20 21:32:37 +0500

    module8-task2

--- js/util.js
+++ js/util.js
@@ -1,4 +1,10 @@
-const randomInteger = (min, max) => Math.floor(Math.random()*(max - min + 1) - min);
+const randomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
 
 const Keys = {
   ESCAPE: 'Escape',
