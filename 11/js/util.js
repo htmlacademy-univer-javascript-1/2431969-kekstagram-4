c1f7f6a52749500323cfabdbaebcd27eb0e71c70
Keks <keks@htmlacademy.ru>
2023-11-27 12:47:50 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/util.js
@@ -0,0 +1,27 @@
+const randomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const Keys = {
+  ESCAPE: 'Escape',
+  ESC: 'Esc'
+};
+
+const isEscapeKey = (evt) => evt.key === Keys.ESCAPE || evt.key === Keys.ESC;
+
+const closeOnEscKeyDown = (evt, cb) => {
+  if (isEscapeKey(evt)) {
+    cb();
+  }
+};
+
+export{randomInteger};
+
+export{closeOnEscKeyDown};
+
+export{isEscapeKey};
+
