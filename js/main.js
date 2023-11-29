da5b607fc4d9b5be4de579da0546f8db8bc60012
helensusanina <alyona.susanina@mail.ru>
2023-11-29 15:43:14 +0500

    module9-task2

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ import {renderPictures} from './pictures.js';
 import './form.js';
 import './hashtags-pristine.js';
 
+import {initEffects} from './effects.js';
+
 renderPictures(data);
+initEffects();
